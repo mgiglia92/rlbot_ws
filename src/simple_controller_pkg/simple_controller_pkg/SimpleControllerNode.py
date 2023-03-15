aae7ebdb17569e8d573565cc8b123f76773a2512
Michael Giglia <michael.a.giglia@gmail.com>
2023-03-15 00:05:12 -0400

    Added Nonlinear Relationships for steering and acceleration

--- src/simple_controller_pkg/simple_controller_pkg/SimpleControllerNode.py
+++ src/simple_controller_pkg/simple_controller_pkg/SimpleControllerNode.py
@@ -4,8 +4,18 @@ from geometry_msgs.msg import Twist, Vector3
 from geometry_msgs.msg import Quaternion as QMsg
 from rlbot_msgs.msg import RigidBodyTick as RigidBodyTickMsg
 import numpy as np
-from pyquaternion import Quaternion as Quaternion
+from pyquaternion import Quaternion
+from controller_util import get_best_steering_and_throttle
+import argparse
 
+parser = argparse.ArgumentParser(
+                    prog='SimplerController',
+                    description='Controls Rlbot Agent',
+                    epilog='No epilog')
+
+parser.add_argument('-v', '--velocity', type=float)
+parser.add_argument('-w', '--angvel', type=float)
+args = parser.parse_args()
 
 class SimpleController(Node):
 
@@ -35,20 +45,22 @@ class SimpleController(Node):
         sign = np.sign(np.dot(body_vel, np.array([1,0,0])))
         body_vel = body_vel     
         vmag = np.linalg.norm(body_vel) * sign
-        kp = 0.001
-        ki = 0.1
-        kd = 0.00001
-        des = 300
+        kp = 1.0
+        ki = 0
+        kd = 0.001
+        des = args.velocity
         err = des-vmag
 
         self.integrand = np.clip(self.integrand + (err*dt/1e9), -1, 1)
 
-        out = kp*(err) + kd*(err - self.prev_err / (dt/1e9)) + ki*self.integrand
+        des_a = kp*(err) + kd*(err - self.prev_err / (dt/1e9)) + ki*self.integrand
+        des_w = args.angvel
+        u_t, u_s = get_best_steering_and_throttle(vmag, des_a, des_w)
         twist = Twist()
-        twist.linear.x = out
-        twist.angular.z = -1.
+        twist.linear.x = u_t
+        twist.angular.z = u_s
         self.publisher_.publish(twist)
-        self.get_logger().info(f"body_vel: {vmag} output: {out}")
+        self.get_logger().info(f"body_vel: {vmag:0.2f} ades:{des_a:0.2f} u_t: {u_t:0.5f} body_w: {msg.bot_state.twist.angular.z:0.2f} wdes:{des_w:0.2f}")
         self.prev_time = tnow
         self.prev_vmag = body_vel
         self.prev_err = err
@@ -56,9 +68,6 @@ class SimpleController(Node):
 
     def norm(self, vec) -> np.array:
         return np.sqrt(vec.x**2 + vec.y**2 + vec.z**2) * np.sign()
-
-def accel_to_throttle(a):
-    return 0
 
 def to_numpy(v):
     try:
---
+++ src/simple_controller_pkg/simple_controller_pkg/controller_util.py
@@ -0,0 +1,73 @@
+from scipy.interpolate import UnivariateSpline
+from geometry_msgs.msg import Vector3, Twist
+import matplotlib.pyplot as plt
+import numpy as np
+
+###------------------------------------------------
+
+class AccelerationRelationship:
+    vel = np.array([0, 1400, 1410, 2300])
+    amax = np.array([1600, 160, 0, 0])
+    veval = np.linspace(0,2300, 501)
+    aeval = np.interp(veval, vel, amax)
+    
+    # Get throttle accelerations max
+    getAmax = UnivariateSpline(veval, aeval, k=5)
+
+class SteeringRelationship:
+    vel = np.array([0, 500, 1000, 1500, 1750, 2300])
+    k = np.array([0.0069, 0.00398, 0.00235, 0.001375, 0.0011, 0.00088])
+    w = vel*k
+    
+    # Spline to get Kmin at diff velocities
+    getKMin = UnivariateSpline(vel, k)
+    # Get Angular velocity max for driving
+    getWMax = UnivariateSpline(vel, w, k=4)
+
+###_----------------------------------------------
+def get_best_steering_and_throttle(vmag, des_a, des_w):
+    # des_a = twist.linear.x
+    # des_w = twist.angular.z
+    a_max = AccelerationRelationship.getAmax(vmag)
+    a_min = -3500
+    w_max = SteeringRelationship.getWMax(vmag)
+    coast = 0.0125
+    print(a_max)
+    if des_a > 0.0:
+        u_t = np.clip(des_a/a_max, coast, 1)
+    elif (des_a >-500) and (des_a < 0):
+        u_t = np.interp(des_a, [-500,0], [-coast, coast])
+    elif des_a <= -500:
+        u_t = np.clip(-1*des_a/a_min, -1, coast)
+    u_s = np.clip(des_w/w_max, -1, 1)
+    return u_t, u_s
+
+def to_numpy(v):
+    try:
+        if type(v) == Vector3:
+            return np.array([v.x, v.y, v.z])
+        if type(v) == QMsg:
+            return np.array([v.w, v.x, v.y, v.z])
+    except:
+        print("Type error")
+        return np.array([0,0,0,0])
+
+if __name__ == "__main__":
+    plt.figure(0)
+    plt.plot(SteeringRelationship.vel, SteeringRelationship.k, 'r.', label='k=f(v)')
+    #Calculate many points using spline
+    vs = np.linspace(0, 2300, 101)
+    ks = SteeringRelationship.getKMin(vs)
+    ws = SteeringRelationship.getWMax(vs)
+    amax = AccelerationRelationship.getAmax(vs)
+    plt.plot(vs,ks,'b.')
+    plt.legend()
+    plt.figure(1)
+    plt.plot(SteeringRelationship.vel, SteeringRelationship.w, 'r*', label = 'w=g(v)')
+    plt.plot(vs, ws, 'b.')
+    plt.legend()
+    plt.figure(2)
+    plt.plot(AccelerationRelationship.vel, AccelerationRelationship.amax)
+    plt.plot(vs, amax, 'b.', label = 'amax = h(v)')
+    plt.legend()
+    plt.show()
