0aee12bf7cdd6f39f8f65d660930337f1b09a7d7
Michael Giglia <michael.a.giglia@gmail.com>
2023-03-15 00:06:45 -0400

    Tweak for setting up venv and ros paths

--- full_setup.bash
+++ full_setup.bash
@@ -4,6 +4,6 @@ echo $1
 export RLBOT_PKG_DIR=/home/mgiglia/workspaces/rlbot_ws/src/rlbot_bridge_pkg
 export RLBOT_AGENT_DIR=/home/mgiglia/workspaces/rlbot_ws/src/rlbot_bridge_pkg/rlbot_bridge_pkg
 echo $RL_BOT_WS_DIR
-source $RLBOT_PKG_DIR/lib/.venv/bin/activate
 source $RLBOT_WS_DIR/install/local_setup.bash
+source $RLBOT_PKG_DIR/lib/.venv/bin/activate
 #cd $RLBOT_AGENT_DIR
