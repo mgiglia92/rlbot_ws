dc0fda6e4e4fdfe704ee9c769fba434cec4a5de8
Michael Giglia <michael.a.giglia@gmail.com>
2023-11-13 23:33:03 -0500

    first iteration of casadi curve fit

--- src/simple_controller_pkg/simple_controller_pkg/casadi_curve_fit.py
+++ src/simple_controller_pkg/simple_controller_pkg/casadi_curve_fit.py
@@ -5,49 +5,69 @@ from rlbot_msgs.msg import PIDGains
 import matplotlib.pyplot as plt
 import numpy as np
 
-deg = 4
-N=501
 
-vel = np.array([0, 1400, 1410, 2300])
-amax = np.array([1600, 160, 0, 0])
-veval = np.linspace(0,2300, N)
-aeval = np.interp(veval, vel, amax)
+from controller_util import AccelerationRelationship
+ar = AccelerationRelationship
 
-#Symbolic expressions
-x = casadi.SX.sym('x')
-y = casadi.SX.sym('y')
-coeff = casadi.SX.sym('coeff', deg+1)
-v = casadi.SX.sym('v', 1, 1)
-fs=[]
-for i in range(deg):
-    fs.append((v**i)*coeff[i])
-f = v*coeff[4] + (v**1)*coeff[3] + (v**2)*coeff[2] + (v**3)*coeff[1] + (v**4)*coeff[0]
-F = casadi.Function('F', [v, coeff], [f])
-dist = casadi.Function('dist', [x,y], [(y-x)**2])
-opti = casadi.Opti()
-coeff = opti.variable(deg+1)
-v = opti.parameter(N)
-a = opti.parameter(N)
+try:
+    deg = 5
+    numcoeff = deg + 1
+    N=501
 
-cinit = [-4.29856970e-11,  4.46795682e-07, -8.19030390e-04, -6.03873482e-01,
-        1.55619827e+03]
-opti.set_initial(coeff, cinit)
+    vel = np.array([0, 1400, 1410, 2300])
+    amax = np.array([1600, 160, 0, 0])
+    veval = np.linspace(0,2300, N)
+    aeval = np.interp(veval, vel, amax)
 
-# for i in range(deg+1):
-    # opti.set_initial(coeff[i], cinit[i])
-for i in range(N):
-    opti.set_value(v[i], veval[i])
-    opti.set_value(a[i], aeval[i])
-    opti.minimize(casadi.norm_1(a[i] - F(v[i],coeff)))
+    #Symbolic expressions
+    x = casadi.SX.sym('x')
+    y = casadi.SX.sym('y')
+    d = casadi.SX.sym('d', numcoeff)
+    c = casadi.SX.sym('c', numcoeff)
+    f = casadi.SX.sym('f', numcoeff)
+    v = casadi.SX.sym('v')
 
-opti.solver('ipopt')
-sol = opti.solve()
+    F = casadi.Function('F', [v, c, d], [c*(v**d)])
+    dist = casadi.Function('dist', [x,y], [(x-y)**2])
 
-plt.figure(0)
-c = sol.value(coeff)
-for i in range(N):
-    plt.plot(veval[i], F(veval[i],c))
-plt.legend()
-plt.show(block=False)
-plt.pause(0.001)
-print("DEBUG")
+    opti = casadi.Opti()
+    c = opti.variable(numcoeff)
+    v = opti.parameter(N)
+    a = opti.parameter(N)
+    d = opti.parameter(numcoeff)
+    vconstrain = opti.parameter(N)
+    aconstrain = opti.parameter(N)
+
+    cinit = np.flip(ar.aCoeffs)
+    opti.set_initial(c, np.polyfit(veval, aeval, deg=deg))
+    # for i in range(len(amax)):
+        # opti.subject_to(opti.bounded(-100, (F(vconstrain[i], c, d) - amax[i])**2, 100))
+        # opti.subject_to(F(vel[i],c,d)==amax[i])
+    for i in range(N):
+        opti.set_value(v[i], veval[i])
+        opti.set_value(a[i], aeval[i])
+    for i in range(len(amax)):
+    #     opti.minimize(casadi.sumsqr((F(vel[i],c, d) - amax[i])))
+        opti.minimize(casadi.sum1(dist(F(vel[i],c,d),amax[i]*np.ones(6).T)))
+    opti.set_value(vconstrain, veval)
+    opti.set_value(aconstrain, aeval)
+    opti.set_value(d, range(numcoeff))
+
+    opti.solver('ipopt')
+    sol = opti.solve()
+
+    plt.figure(0)
+    cval = sol.value(c)
+    plt.plot(veval, aeval, 'b.')
+    plt.plot(veval, AccelerationRelationship.getAmax(veval), 'g.')
+    plt.plot(veval, np.polyval(cval,veval), 'r.')
+    plt.show(block=False)
+    # plt.show()
+    plt.pause(0.001)
+    print(ar.aCoeffs-cval)
+    print("DEBUG")
+except Exception as e:
+    import traceback
+    print(f"{traceback.print_exc()}")
+    print("DEBUG")
+    
