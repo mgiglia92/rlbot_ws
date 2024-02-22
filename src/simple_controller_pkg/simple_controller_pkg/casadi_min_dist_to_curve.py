b3a1daf1de07f6a1c0e2aca1f98f8ca4b8cffda8
Michael Giglia <michael.a.giglia@gmail.com>
2024-02-22 12:30:11 -0500

    add constraints to solution to prevent errors

--- src/simple_controller_pkg/simple_controller_pkg/casadi_min_dist_to_curve.py
+++ src/simple_controller_pkg/simple_controller_pkg/casadi_min_dist_to_curve.py
@@ -10,55 +10,43 @@ import numpy as np
 # ar = AccelerationRelationship
 
 try:
-    # deg = 5
-    # numcoeff = deg + 1
-    # N=501
-
-    # vel = np.array([0, 1400, 1410, 2300])
-    # amax = np.array([1600, 160, 0, 0])
-    # veval = np.linspace(0,2300, N)
-    # aeval = np.interp(veval, vel, amax)
-
-    # Robot Position
-    x=1.1
-    y=1
 
     #Symbolic expressions
     xc = casadi.SX.sym('xc')
     x1 = casadi.SX.sym('x1')
     yc = casadi.SX.sym('yc')
     y1 = casadi.SX.sym('y1')
-    # d = casadi.SX.sym('d', numcoeff)
-    # c = casadi.SX.sym('c', numcoeff)
-    # f = casadi.SX.sym('f', numcoeff)
-    # v = casadi.SX.sym('v')
-
-    # F = casadi.Function('F', [v, c, d], [c*(v**d)])
-    F = casadi.Function('F', [xc], [casadi.sqrt(1-xc**2)])
+    F = casadi.Function('F', [xc], [casadi.sqrt(10000-xc**2)])
     dist = casadi.Function('dist', [xc, x1, yc, y1], [casadi.sqrt((xc-x1)**2 + (yc-y1)**2)])
 
     opti = casadi.Opti()
+    xpos = opti.parameter()
+    ypos = opti.parameter()
     xmin = opti.variable(1)
     ymin = F(xmin)
     opti.set_initial(xmin, 0.99)
-    opti.minimize(dist(xmin, x, ymin, y))
-    opti.solver('ipopt')
+    opti.subject_to(xmin>=-99)
+    opti.subject_to(xmin<=99)
+    opti.minimize(dist(xmin, xpos, ymin, ypos))
+    opts = {}#{'ipopt.print_level': 2, 'print_time': 0, 'ipopt.sb': 'yes'}
+    opti.solver('ipopt', opts)
     xsol=[]
     ysol=[]
     xp=[]
     yp=[]
 
-    for i in range(1,10):
-        x=(i/10)
-        y=2*((i/10)**2)
-        xp.append(x)
-        yp.append(y)
-        opti.minimize(dist(xmin, x, ymin, y))
-        sol = opti.solve()
-        xsol.append(opti.value(xmin))
-        ysol.append(opti.value(ymin))
+    # for i in range(1,10):
+    x=196
+    y=-507
+    opti.set_value(xpos,x)
+    opti.set_value(ypos, y)
+    xp.append(x)
+    yp.append(y)
+    sol = opti.solve()
+    xsol.append(opti.value(xmin))
+    ysol.append(opti.value(ymin))
     plt.figure(0)
-    xcurve = np.linspace(-1, 1, 101)
+    xcurve = np.linspace(-100, 100, 101)
     ycurve = F(xcurve)
     plt.plot(xcurve, ycurve, 'b.')
     plt.plot(xp, yp, 'g.')
